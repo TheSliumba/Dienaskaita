e68061ef7a64bfc1f57bc2dfce849bf7d4a4e5b7
TheSliumba <asliumbaz@gmail.com>
2020-05-28 13:04:28 +0300

    Additional functionality added, styling still lacking, still needs doing.

--- skriptas.js
+++ skriptas.js
@@ -2,19 +2,38 @@ var slider = document.getElementById("dayScore");
 var output = document.getElementById("dayValue");
 output.innerHTML = slider.value; // Display the default slider value
 
-// Update the current slider value (each time you drag the slider handle)
+
+// Update the current slider value and text (each time you drag the slider handle)
 slider.oninput = function() {
   output.innerHTML = this.value;
-  //Now change text color according to slider value
+  //Now change text according to slider value
   if (this.value < 25){
-    document.getElementById("sampletext").style.color = "red"
+    document.getElementById("sampletext_bad").style.display = "block";
+    document.getElementById("sampletext_good").style.display = "none";
+    document.getElementById("sampletext_ok").style.display = "none"
   } else if (this.value > 75){
-    document.getElementById("sampletext").style.color = "green"
+    document.getElementById("sampletext_bad").style.display = "none";
+    document.getElementById("sampletext_good").style.display = "block";
+    document.getElementById("sampletext_ok").style.display = "none"
   } else {
-    document.getElementById("sampletext").style.color = "cornflowerblue"
-  }
+    document.getElementById("sampletext_bad").style.display = "none";
+    document.getElementById("sampletext_good").style.display = "none";
+    document.getElementById("sampletext_ok").style.display = "block"
+  } // There's probably a much better and compact way to write this up, but it'll do for now
 }
+
+
 //Trying to put in current date into text so I don't have to change manually
 var today = new Date()
 var weekdays = ["sekmadienį", "pirmadienį", "antradienį", "trečiadienį", "ketvirtadienį", "penktadienį", "šeštadienį"]
 document.getElementById("today").innerHTML = weekdays[today.getDay()]
+
+
+//So now Imma try to make a button press dish out a (hopefully) random string in index
+//To start, I need to figure out how to make the submit action activate the script to print
+function randomise_text(){
+  var possibilities = ["Pavalgyt", "Parašyt", "Pakodint kokią valandžikę",
+  "Pageimint", "Prasieit rajone", "Paiskrapštyt prie mašinos", "Paieškot pen-pal'ų"]
+  var choice = Math.floor(Math.random() * possibilities.length)
+  document.getElementById("doables").innerHTML = possibilities[choice]
+}
